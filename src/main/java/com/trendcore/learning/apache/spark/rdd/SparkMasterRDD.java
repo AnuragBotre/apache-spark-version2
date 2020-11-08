38b2e976c8bd4681749f635eadb1d918147153cb
anurag <anurag.botre.202@gmail.com>
2020-11-08 16:30:38 +0530

    Refactoring SparkMasterRDD.

--- src/main/java/com/trendcore/learning/apache/spark/rdd/SparkMasterRDD.java
+++ src/main/java/com/trendcore/learning/apache/spark/rdd/SparkMasterRDD.java
@@ -21,10 +21,10 @@ public class SparkMasterRDD {
         JavaSparkContext sparkContext = new JavaSparkContext(wordCountSparkConf);
 
         List<Integer> integers = Arrays.asList(1, 2, 3 ,4 ,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
-        RDD<Integer> trdd = ToRDD.toRdd(sparkContext.sc(), integers, ClassManifestFactory.<Integer>classType(Integer.class));
+        RDD<Integer> trdd = ToRDD.toRdd(sparkContext.sc(), integers, ClassManifestFactory.classType(Integer.class));
 
 
-        JavaRDD<Integer> integerJavaRDD = JavaRDD.fromRDD(trdd, ClassManifestFactory.<Integer>classType(Integer.class));
+        JavaRDD<Integer> integerJavaRDD = JavaRDD.fromRDD(trdd, ClassManifestFactory.classType(Integer.class));
 
         JavaRDD<Integer> repartition = integerJavaRDD.repartition(5);
 
