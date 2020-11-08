256789cce3ebeca41e0475cc6962b2d519546c0c
anurag <anurag.botre.202@gmail.com>
2020-11-08 16:26:42 +0530

    Initial commit

---
+++ src/main/scala/com/trendcore/learning/apache/spark/ToRDD.scala
@@ -0,0 +1,17 @@
+package com.trendcore.learning.apache.spark
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+import scala.collection.JavaConversions._
+import scala.reflect.ClassTag
+
+object ToRDD {
+
+  def toRdd[T: ClassTag](sparkContext: SparkContext, list: java.util.List[T]): RDD[T] = {
+    val seq = list.toSeq
+    val rdd = sparkContext.parallelize(seq)
+    rdd
+  }
+
+}
