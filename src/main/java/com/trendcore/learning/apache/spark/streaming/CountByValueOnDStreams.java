16cf56e114168641105b51c5f5168ddf21a05ee4
anurag <anurag.botre.202@gmail.com>
2021-01-23 20:57:21 +0530

    Count by value on DStreams

--- src/main/java/com/trendcore/learning/apache/spark/streaming/CountByValueOnDStreams.java
+++ src/main/java/com/trendcore/learning/apache/spark/streaming/CountByValueOnDStreams.java
@@ -9,7 +9,9 @@ import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 public class CountByValueOnDStreams {
 
@@ -43,7 +45,7 @@ public class CountByValueOnDStreams {
                 ;
 
         stringLongJavaPairDStream.foreachRDD((rdd, timeUnits) -> {
-            System.out.println("TimeUnits :- " + timeUnits);
+            System.out.println("TimeUnits :- " + new SimpleDateFormat("dd-M-yyyy hh:mm:ss").format(new Date(timeUnits.milliseconds())));
             rdd.foreachPartition(tuple2Iterator -> {
                 tuple2Iterator.forEachRemaining(stringLongTuple2 -> {
                     System.out.println(stringLongTuple2._1 + " " + stringLongTuple2._2);
